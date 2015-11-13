48fb1732b62ba842205fd0b92b42e74ac54f29f1
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-12 22:03:04 -0800

    Allow client to post messages

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,19 @@ $(document).ready(function() {
   }
 
   function post() {
-    
+    var msgVal = $('#messageArea').val();
+
+    if (msgVal) {
+      var message = {
+        author: $('#author').val() || 'anonymous',
+        time: Date.now(),
+        message: msgVal
+      }
+
+      $.post('/messages', message)
+      .done(() => location.reload(true))
+      .fail(err => console.log("ERROR POSTING MESSAGE:", err));
+    }
   }
 
 })
