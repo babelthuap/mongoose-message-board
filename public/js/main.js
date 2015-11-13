da6d66b02b09918052b10179324f86ae3e520a2a
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-13 10:49:15 -0800

    Make the frontend work with ES5

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,19 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   $('#new').click(getNewMessage);
   $('#board').on('click', '.remove', removeMessage);
   $('#board').on('click', '.edit', editMessage);
 
-
   function getNewMessage() {
     $('#post').off('click');
     $('#author, #messageArea').val('');
     $('.modal-title').text('Post New Message');
-    $('#post').text('Post').click(function() {
+    $('#post').text('Post').click(function () {
       modifyDB('POST');
     });
   }
-
 
   function modifyDB(method, id) {
     var msgVal = $('#messageArea').val();
@@ -23,36 +21,35 @@ $(document).ready(function() {
     if (msgVal) {
       var message = {
         author: $('#author').val() || 'anonymous',
-        time: Date.now(),
         message: msgVal
-      }
+      };
 
       if (id) message['_id'] = id;
 
       $.ajax({
         method: method,
-        url: '/messages', 
+        url: '/messages',
         data: message
-      })
-      .done(() => location.reload(true))
-      .fail(err => console.log("ERROR POSTING MESSAGE:", err));
+      }).done(function () {
+        return location.reload(true);
+      }).fail(function (err) {
+        return console.log("ERROR POSTING MESSAGE:", err);
+      });
     }
   }
-
 
   function editMessage() {
     $('#post').off('click');
     var id = $(this).siblings('.mongoId').text();
 
-    $('#author').val( $(this).siblings('#msgAuthor').text() );
-    $('#messageArea').val( $(this).closest('.row').find('p').text() );
+    $('#author').val($(this).siblings('#msgAuthor').text());
+    $('#messageArea').val($(this).closest('.row').find('p').text());
 
     $('.modal-title').text('Edit Message');
-    $('#post').text('Update').click(function() {
+    $('#post').text('Update').click(function () {
       modifyDB('PUT', id);
     });
   }
-
 
   function removeMessage() {
     var id = $(this).siblings('.mongoId').text();
@@ -60,10 +57,11 @@ $(document).ready(function() {
     $.ajax({
       method: 'DELETE',
       url: '/messages',
-      data: {'_id': id}
-    })
-    .done(() => location.reload(true))
-    .fail(err => console.log("ERROR DELETING MESSAGE:", err));
+      data: { '_id': id }
+    }).done(function () {
+      return location.reload(true);
+    }).fail(function (err) {
+      return console.log("ERROR DELETING MESSAGE:", err);
+    });
   }
-
-})
+});
