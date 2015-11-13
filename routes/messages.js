9e1c6b2fcf36122c706e9ba51a237e0fc29046b1
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-13 08:37:08 -0800

    Use moment.js to render dates

--- routes/messages.js
+++ routes/messages.js
@@ -11,20 +11,20 @@ router.get('/', function(req, res) {
   Message.find({}, function(err, messages) {
     // sort - newest messages on top
     messages.sort((msg1, msg2) => msg2.time - msg1.time);
-    res.render("index", {messages: messages});
+    var moment = require('moment')
+    res.render("index", {messages: messages, moment: moment});
   });
 });
 
-
+// not used currently, but good to have nonetheless
 router.get('/all', function(req, res) {
   Message.find({}, function(err, messages) {
     res.send(messages);
   });
 });
 
-
 router.post('/', function(req, res) {
-  var message = new Message(req.body); // uppercase is the 'message class'
+  var message = new Message(req.body);
   message.save(function(err, savedMessage) {
     res.send(savedMessage);
   });
