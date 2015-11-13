32c01ce6971173e2179f827ea8627360d032e5a5
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-13 10:50:12 -0800

    Update schema to have required fields and a default for time

--- models/message.js
+++ models/message.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose');
 
 // define the schema
 var messageSchema = mongoose.Schema({
-  author: String,
-  time: Number,
+  author: { type: String, required: true },
+  time: { type: Number, required: true, default: Date.now() },
   message: String
 });
 
--- routes/messages.js
+++ routes/messages.js
@@ -11,34 +11,34 @@ router.get('/', function(req, res) {
   Message.find({}, function(err, messages) {
     // sort - newest messages on top
     messages.sort((msg1, msg2) => msg2.time - msg1.time);
-    var moment = require('moment')
-    res.render("index", {messages: messages, moment: moment});
+    var moment = require('moment');
+    res.status(err ? 400 : 200).render("index", {messages: messages, moment: moment});
   });
 });
 
 // not used currently, but good to have nonetheless
 router.get('/all', function(req, res) {
   Message.find({}, function(err, messages) {
-    res.send(messages);
+    res.status(err ? 400 : 200).send(err || messages);
   });
 });
 
 router.post('/', function(req, res) {
   var message = new Message(req.body);
   message.save(function(err, savedMessage) {
-    res.send(savedMessage);
+    res.status(err ? 400 : 200).send(err || savedMessage);
   });
 });
 
 router.put('/', function(req, res) {
   Message.findByIdAndUpdate(req.body._id, req.body, function(err, message) {
-    res.send('updated!');
+    res.status(err ? 400 : 200).send(err || 'updated!');
   });
 });
 
 router.delete('/', function(req, res) {
   Message.findByIdAndRemove(req.body._id, function(err, message) {
-    res.send('deleted!');
+    res.status(err ? 400 : 200).send(err || 'deleted!');
   });
 });
 
